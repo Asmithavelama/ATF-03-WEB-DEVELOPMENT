776e56d8d6f1bc0e546bd042d8326971d398021d
AsmithaVelama <asmithavelama2244@gmail.com>
2024-09-11 10:54:48 +0530

    intial

---
+++ fun.js
@@ -0,0 +1,16 @@
+let a = prompt("Enter input1");
+let b= prompt("Enter input2");
+let c = prompt("enter the operation");
+
+switch(c){
+    case "+":
+     alert(a + b);
+    case "-":
+     alert(a -b);
+    case "*":
+     alert(a*b);
+     default:sssss;
+}     
+console.log(a);
+console.log(b);
+console.log(c);
